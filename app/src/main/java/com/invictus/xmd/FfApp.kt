cb23dbc3493005718e38f369b9db0078dbf4db2a
Arnab Sadhukhan <arnab@Arnabs-Mac-Mini.local>
2026-09-05 21:33:42 +0530

    feat(debug): integrate LeakCanary for memory leak detection

--- app/src/main/java/com/invictus/xmd/FfApp.kt
+++ app/src/main/java/com/invictus/xmd/FfApp.kt
@@ -1,9 +1,14 @@
 package com.invictus.xmd
 
+import android.app.Activity
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.invictus.xmd.domain.browser.AdblockFilter
 import com.invictus.xmd.preferences.Settings
 import com.invictus.xmd.repository.BookmarkRepository
@@ -12,7 +17,7 @@ import com.invictus.xmd.repository.QueueRepository
 import com.invictus.xmd.repository.ShortcutRepository
 import com.invictus.xmd.utils.FaviconLoader
 
-class FfApp : Application() {
+class FfApp : Application(), Application.ActivityLifecycleCallbacks {
 
     companion object {
         const val DOWNLOAD_CHANNEL_ID = "ff_downloads"
@@ -20,6 +25,7 @@ class FfApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        registerActivityLifecycleCallbacks(this)
         Settings.init(this)
         AdblockFilter.init(this)
         // Loads the previously-persisted queue from disk so it survives
@@ -42,4 +48,30 @@ class FfApp : Application() {
             manager.createNotificationChannel(channel)
         }
     }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        if (activity.javaClass.name.contains("leakcanary", ignoreCase = true)) {
+            val rootView = activity.findViewById<View>(android.R.id.content)
+            rootView?.let { view ->
+                ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+                    val statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+                    val navBarInsets = insets.getInsets(WindowInsetsCompat.Type.navigationBars())
+                    v.setPadding(
+                        v.paddingLeft,
+                        statusBarInsets.top,
+                        v.paddingRight,
+                        navBarInsets.bottom,
+                    )
+                    insets
+                }
+            }
+        }
+    }
+
+    override fun onActivityStarted(activity: Activity) = Unit
+    override fun onActivityResumed(activity: Activity) = Unit
+    override fun onActivityPaused(activity: Activity) = Unit
+    override fun onActivityStopped(activity: Activity) = Unit
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+    override fun onActivityDestroyed(activity: Activity) = Unit
 }
